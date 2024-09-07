b5c80337c0d3cc48ba3f65eb08f57fe26006af7e
vinayaksaubhri <vinayaksaubhri@gmail.com>
2024-09-07 14:03:35 +0530

    feat:useHomeData hook

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import { HomesProvider } from "./hooks/useHomeData";
+import HomeScreen from "./screen/homeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomesProvider>
+      <HomeScreen />
+    </HomesProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ hooks/useHomeData.tsx
@@ -0,0 +1,55 @@
+import { createContext, useContext, useState } from "react";
+import homesData from "../assets/homes.json"; // Mock data
+import { generateDelay } from "../utils";
+
+type homeDataType = {
+  id: number;
+  address: string;
+  description: string;
+  imageUrl: string;
+  coordinates: {
+    latitude: number;
+    longitude: number;
+  };
+};
+
+const HomeContext = createContext<{
+  homes: homeDataType[];
+  fetchHomes: () => void;
+  loading: boolean;
+}>({
+  homes: [],
+  fetchHomes: () => {},
+  loading: false,
+});
+
+export function HomesProvider({ children }: { children: React.ReactNode }) {
+  const [homes, setHomes] = useState<[] | homeDataType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchHomes = async () => {
+    setLoading(true);
+    try {
+      generateDelay(2000); // Simulate network delay
+      setHomes(homesData);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
+  return (
+    <HomeContext.Provider
+      value={{
+        homes,
+        fetchHomes,
+        loading,
+      }}
+    >
+      {children}
+    </HomeContext.Provider>
+  );
+}
+
+export const useHomeData = () => {
+  return useContext(HomeContext);
+};
